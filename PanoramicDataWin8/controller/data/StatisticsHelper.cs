4e1591c909b7729d8ed4b3006c8331c5d942b16c
ezg <emanuel.zgraggen@gmail.com>
2016-02-09 10:52:39 -0500

    updates

---
+++ PanoramicDataWin8/controller/data/StatisticsHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanoramicDataWin8.controller.data
+{
+    public static class StatisticsHelper
+    {
+        //public static double RunningSMA()
+    }
+}
